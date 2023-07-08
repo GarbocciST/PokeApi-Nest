17940d3b487cff126eeb6b977c7dbb9b65297a10
Stefano Garbocci <stefgarbocci@gmail.com>
2023-07-08 17:03:50 -0300

    feat(pokemon.service): Agregado update pokemons

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -26,9 +26,7 @@ export class PokemonService {
       return pokemon;
     } catch (error) {
 
-      if (error.code === 11000) throw new BadRequestException(`Pokemon already exists ${ JSON.stringify(error.keyValue) }`);
-      
-      throw new InternalServerErrorException(`Can't create pokemon - Check server logs`);
+      this.handleException(error, 'create');
 
     }
 
@@ -42,32 +40,44 @@ export class PokemonService {
     
     let pokemon : Pokemon;
     
-    if ( !isNaN( +term ) ) {
-      pokemon = await this.pokemonModel.findOne({ no: term});
-    } 
+    if ( !isNaN( +term )) pokemon = await this.pokemonModel.findOne({ no: term});
 
     // Mongo
-    if ( !pokemon && isValidObjectId(term) ) {
-      pokemon = await this.pokemonModel.findById(term);
-    }
+    if ( !pokemon && isValidObjectId(term)) pokemon = await this.pokemonModel.findById(term);
 
     // Name 
-    if (!pokemon ) {
-      pokemon = await this.pokemonModel.findOne({ name: term.toLocaleLowerCase().trim() });
-    }
+    if (!pokemon ) pokemon = await this.pokemonModel.findOne({ name: term.toLocaleLowerCase().trim() });
 
-    if ( !pokemon ) {
-      throw new BadRequestException(`Invalid term ${term}`)
-    }
+    if ( !pokemon ) throw new BadRequestException(`Invalid term ${term}`) 
 
     return pokemon;
   }
 
-  async update(id: number, updatePokemonDto: UpdatePokemonDto) {
-    return `This action updates a #${id} pokemon`;
+  async update(term: string, updatePokemonDto: UpdatePokemonDto) {
+
+    try {
+      const pokemon = await this.findOne(term);
+    
+      if( updatePokemonDto.name ) updatePokemonDto.name = updatePokemonDto.name.toLowerCase();
+  
+      await pokemon.updateOne( updatePokemonDto, {new: true} )
+      
+      return {...pokemon.toJSON(), ...updatePokemonDto} ;
+
+    } catch (error) {
+      this.handleException(error, 'update');
+    }
   }
 
   async remove(id: number) {
     return `This action removes a #${id} pokemon`;
   }
+
+
+  private handleException(error: any, message: string) {
+    if (error.code === 11000) throw new BadRequestException(`Pokemon already exists ${ JSON.stringify(error.keyValue) }`);
+      
+    throw new InternalServerErrorException(`Can't ${message} pokemon - Check server logs`);
+  }
+
 }
